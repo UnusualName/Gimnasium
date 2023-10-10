25e8242a8157aafcf7837bc7e4ca58a2e9ef9373
Aramarchuk <aramarchuk42@gmail.com>
2023-10-10 17:42:17 +0500

    Quotes

--- lyceum/lyceum/urls.py
+++ lyceum/lyceum/urls.py
@@ -1,4 +1,4 @@
-"""
+'''
 URL configuration for lyceum project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -13,10 +13,10 @@ Class-based views
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+'''
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path('admin/', admin.site.urls),
 ]
