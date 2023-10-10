7b55e82818f46e9ca5bed89d8a8d8659be921fb9
Aramarchuk <aramarchuk42@gmail.com>
2023-10-10 19:46:28 +0500

    mmm

--- lyceum/manage.py
+++ lyceum/manage.py
@@ -12,7 +12,8 @@ def main():
     except ImportError as exc:
         raise ImportError(
             "available on your PYTHONPATH environment variable? Did you"
-            "forget to activate a virtual environment?") from exc
+            "forget to activate a virtual environment?"
+        ) from exc
     execute_from_command_line(sys.argv)
 
 
