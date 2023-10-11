12504822b433b7e379026717cc4c92bd2a0ac59b
Aramarchuk <aramarchuk42@gmail.com>
2023-10-11 17:01:38 +0500

    sss

--- lyceum/lyceum/settings.py
+++ lyceum/lyceum/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
-AUTH_PASSWORD_VALIDATORS = AUTH_PASSWORD_VALIDATORS = [
+AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": (
             "django.contrib.auth.password_validation"
@@ -96,20 +96,17 @@ AUTH_PASSWORD_VALIDATORS = AUTH_PASSWORD_VALIDATORS = [
     },
     {
         "NAME": (
-            "django.contrib.auth.password_validation" 
-            ".MinimumLengthValidator"
+            "django.contrib.auth.password_validation.MinimumLengthValidator"
         ),
     },
     {
         "NAME": (
-            "django.contrib.auth.password_validation"
-            ".CommonPasswordValidator"
+            "django.contrib.auth.password_validation.CommonPasswordValidator"
         ),
     },
     {
         "NAME": (
-            "django.contrib.auth.password_validation"
-            ".NumericPasswordValidator"
+            "django.contrib.auth.password_validation.NumericPasswordValidator"
         ),
     },
 ]
