16c221474982849114f0feafa6dc95bde9ae5c77
Aramarchuk <aramarchuk42@gmail.com>
2023-10-13 03:24:00 +0500

    tests and pip

--- lyceum/homepage/tests.py
+++ lyceum/homepage/tests.py
@@ -1,6 +1,6 @@
 from http import HTTPStatus
 
-from django.test import Client, TestCase
+from django.test import Client, override_settings, TestCase
 
 
 class StaticUrlTests(TestCase):
@@ -15,3 +15,11 @@ class StaticUrlTests(TestCase):
     def test_coffee_httpstatus_endpoint(self):
         response = Client().get("/coffee/")
         self.assertEqual(response.status_code, HTTPStatus.IM_A_TEAPOT)
+
+    @override_settings(ALLOW_REVERSE=False)
+    def test_coffee_text_endpoint(self):
+        response = Client().get("/coffee/")
+        self.assertEqual(
+            response.content,
+            "Я чайник".encode(),
+        )
