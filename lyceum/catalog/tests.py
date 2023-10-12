336b30762e020dcb5234f183cc62b352fbd8fa8c
Aramarchuk <aramarchuk42@gmail.com>
2023-10-12 17:02:21 +0500

    adding tests

--- lyceum/catalog/tests.py
+++ lyceum/catalog/tests.py
@@ -12,8 +12,16 @@ class StaticUrlTests(TestCase):
 
     def test_catalog_regular_positive_endpoint(self):
         response = Client().get("/catalog/re/-1234/")
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 404)
 
     def test_catalog_regular_text_endpoint(self):
         response = Client().get("/catalog/re/12r34/")
         self.assertEqual(response.status_code, 404)
+
+    def test_catalog_converter_positive_endpoint(self):
+        response = Client().get("/catalog/re/-1234/")
+        self.assertEqual(response.status_code, 404)
+
+    def test_catalog_converter_text_endpoint(self):
+        response = Client().get("/catalog/re/12r34/")
+        self.assertEqual(response.status_code, 200)
