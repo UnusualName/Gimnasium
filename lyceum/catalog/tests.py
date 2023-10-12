ba13431b54c9a5d364902f630d8f8b3e7c212882
Aramarchuk <aramarchuk42@gmail.com>
2023-10-12 15:39:59 +0500

    test fix

--- lyceum/catalog/tests.py
+++ lyceum/catalog/tests.py
@@ -7,5 +7,5 @@ class StaticUrlTests(TestCase):
         self.assertEqual(response.status_code, 200)
 
     def test_catalog_item_endpoint(self):
-        response = Client().get("/catalog/1234")
+        response = Client().get("/catalog/1234/")
         self.assertEqual(response.status_code, 200)
--- lyceum/catalog/urls.py
+++ lyceum/catalog/urls.py
@@ -5,5 +5,5 @@ from . import views
 
 urlpatterns = [
     path("", views.item_list),
-    path("<int:item_n>", views.item_detail),
+    path("<int:item_n>/", views.item_detail),
 ]
