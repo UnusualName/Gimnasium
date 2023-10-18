162df11f66deee1bec54f523ac39cb1d04b8d378
Aramarchuk <aramarchuk42@gmail.com>
2023-10-19 00:44:30 +0500

    tests

--- lyceum/catalog/tests.py
+++ lyceum/catalog/tests.py
@@ -1,5 +1,7 @@
 from http import HTTPStatus
 
+from catalog import models
+import django.core
 from django.test import Client, TestCase
 from parameterized import parameterized
 
@@ -35,3 +37,63 @@ class StaticUrlTests(TestCase):
     def test_catalog_negative_endpoint(self, item, excepted):
         response = Client().get(f"/catalog{item}/")
         self.assertEqual(response.status_code, excepted)
+
+
+class StaticModelTests(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.category = models.Category.objects.create(
+            is_published=True,
+            name="Тестовая категория",
+            slug="test-category-slug",
+            weight=100,
+        )
+        print(cls.category)
+        cls.tag = models.Tag.objects.create(
+            is_published=True, name=" Тестовый тег", slug="test-tag-slug"
+        )
+
+    @parameterized.expand(
+        [
+            ("Without", "Not Prevoshodno"),
+            ("Empty", ""),
+            ("Splitted", "П р е в о с х о д н о"),
+        ]
+    )
+    def test_create_perfection_validators_negative(self, names, texts):
+        item_count = models.Item.objects.count()
+        with self.assertRaises(django.core.exceptions.ValidationError):
+            self.item = models.Item(
+                name=names,
+                text=texts,
+                category=StaticModelTests.category,
+            )
+            self.item.full_clean()
+            self.item.save()
+            self.item.tags.add(StaticModelTests.tag)
+            self.item.tags.save()
+        self.assertEqual(models.Item.objects.count(), item_count)
+
+    @parameterized.expand(
+        [
+            ("test-name", "Превосходно"),
+            ("0", "Восхитительно и Превосходно"),
+            ("0", "Не только превосходно"),
+            ("0", "ПреПреПревосходно"),
+        ]
+    )
+    def test_create_item_positive(self, names, texts):
+        item_count = models.Item.objects.count()
+        self.item = models.Item(
+                name=names,
+                text=texts,
+                category=StaticModelTests.category,
+            )
+        self.item.full_clean()
+        self.item.save()
+        self.item.tags.add(StaticModelTests.tag)
+        self.assertEqual(models.Item.objects.count(), item_count+1)
+        self.item.delete()
+        self.item.save()
+        self.assertEqual(models.Item.objects.count(), item_count)
